e568164c9f2f42e57da288d276a5298deb5576d2
Michael Chen <michaelchengaming@gmail.com>
2021-01-25 06:35:09 -0800

    install axios, add functionality to nbastats cmd

--- commands/sports/nbaStats.js
+++ commands/sports/nbaStats.js
@@ -1,4 +1,6 @@
 const { Command } = require('discord.js-commando');
+const { MessageEmbed } = require('discord.js');
+const axios = require('axios').default;
 
 module.exports = class NbaStatsCommand extends (
   Command
@@ -13,7 +15,65 @@ module.exports = class NbaStatsCommand extends (
   }
 
   async run(message, args) {
-    console.log(args);
-    return message.say('testing! ' + args);
+    if (!args.length)
+      return message.say(
+        "You didn't provide any arguments. Try `!nbastats [First Name] [Last Name]` "
+      );
+    let player = await fetchPlayer(args);
+    let playerStatMsg = await buildPlayer(player);
+    return message.say(playerStatMsg);
   }
+};
+
+// fetch player object with name, id, height, weight, etc
+const fetchPlayer = async (name) => {
+  try {
+    let res = await axios.get(`https://www.balldontlie.io/api/v1/players?search=${name}`);
+    let { data } = res.data;
+    return data[0];
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+// retrieves json with player stats current season
+const fetchPlayerStats = async (id) => {
+  try {
+    let res = await axios.get(
+      `https://www.balldontlie.io/api/v1/season_averages?player_ids[]=${id}`
+    );
+    let { data } = res.data;
+    return data[0];
+  } catch (error) {
+    console.log(error.message);
+    return error.message;
+  }
+};
+
+// builds the message for the requested player
+const buildPlayer = async (playerJson) => {
+  let stats = await fetchPlayerStats(playerJson.id);
+  console.log(stats);
+  if (stats === undefined) return "This player doesn't have stats for this season.";
+
+  return new MessageEmbed()
+    .setColor('#234134')
+    .setTitle(`Stats for ${playerJson.first_name + ' ' + playerJson.last_name}`)
+    .setDescription(
+      `
+  Height: ${playerJson.height_feet + "'" + playerJson.height_inches + '"'}, Weight: ${
+        playerJson.weight_pounds
+      }
+    Games Played: ${stats.games_played}, Avg Mins: ${stats.min}
+    FG%: ${stats.fg_pct}, FG3%: ${stats.fg3_pct}, FT%: ${stats.ft_pct}
+    Points: ${stats.pts}
+    Rebound: ${stats.reb}
+    Assist: ${stats.ast}
+    Steals: ${stats.stl}
+    Blocks: ${stats.blk}
+    Turnovers: ${stats.turnover}
+  `
+    );
+  // .addField('\u200b', '\u200b');
+  // .addField('Inline field title', 'Some value here', true);
 };
